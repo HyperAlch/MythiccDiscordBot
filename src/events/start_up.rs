ed95371b56648ef58152586d0e8ddef05c6d7c11
HyperAlch <Daemonleak@protonmail.com>
2022-12-07 16:34:15 -0500

    Version 1.1: Deleted unneeded startup code

--- src/events/start_up.rs
+++ src/events/start_up.rs
@@ -1,7 +1,6 @@
 use serenity::client::Context;
 use serenity::model::gateway::Ready;
 use serenity::model::id::GuildId;
-use serenity::model::permissions::Permissions;
 use serenity::model::prelude::{ChannelId, GuildChannel, Role, RoleId};
 
 use crate::slash_commands as sc;
@@ -15,7 +14,7 @@ use crate::redis_client::{self, check_master_admin, set_guild_id};
 struct LocalGuild {
     role_list: HashMap<RoleId, Role>,
     channel_list: HashMap<ChannelId, GuildChannel>,
-    guild_id: GuildId,
+    // guild_id: GuildId,
 }
 
 impl LocalGuild {
@@ -33,19 +32,8 @@ impl LocalGuild {
         Self {
             role_list: guild_roles,
             channel_list: guild_channels,
-            guild_id: *guild_id,
+            // guild_id: *guild_id,
         }
-    }
-
-    fn role_exists_from_str(&self, role_id: &String) -> bool {
-        let role_id_u64: u64 = role_id
-            .parse()
-            .expect(&format!("{} cannot be parsed into u64", role_id)[..]);
-        self.role_exists_from_u64(&role_id_u64)
-    }
-
-    fn role_exists_from_u64(&self, role_id: &u64) -> bool {
-        self.role_exists(&RoleId(*role_id))
     }
 
     fn role_exists(&self, role_id: &RoleId) -> bool {
@@ -84,7 +72,6 @@ pub async fn handle(ctx: Context, ready: Ready) {
 
     let guild = LocalGuild::new(&guild_id, &ctx).await;
 
-    guild.check_bot_admin_role(&mut connection, &ctx).await;
     guild.check_follower_role(&mut connection).await;
     guild.check_log_channel(&mut connection).await;
 
@@ -115,65 +102,6 @@ async fn register_commands(ctx: &Context, guild_id: &GuildId) {
 }
 
 impl LocalGuild {
-    async fn check_bot_admin_role(&self, connection: &mut redis::Connection, ctx: &Context) {
-        // Attempt to query from Redis
-        let bot_admin_role = redis_client::get_bot_role(connection);
-        match bot_admin_role {
-            // If the redis query was successful
-            Ok(role_id) => match role_id {
-                Some(role_id) => {
-                    if self.role_exists_from_str(&role_id) {
-                        println!("Bot Admin Role Found: {}", role_id);
-                    } else {
-                        self.create_bot_admin_role(connection, &ctx).await;
-                    }
-                }
-                None => self.create_bot_admin_role(connection, &ctx).await,
-            },
-            // If the redis query fails
-            Err(e) => panic!("{}", e),
-        }
-    }
-
-    async fn create_bot_admin_role(&self, connection: &mut redis::Connection, ctx: &Context) {
-        println!("Starting Bot Admin Role Creation Process...");
-
-        // Get all the roles in the guild
-        let roles_amount = self
-            .guild_id
-            .roles(&ctx.http)
-            .await
-            .expect("Could not get roles for guild");
-
-        // Find all the administrator roles, and figure out the one lowest on the list
-        let mut lowest_admin_position = roles_amount.len();
-        for (_, r) in roles_amount {
-            if r.permissions.administrator() {
-                if r.position < lowest_admin_position as i64 {
-                    lowest_admin_position = r.position as usize;
-                }
-            };
-        }
-
-        // Put the `MythiccBot` role directly under the lowest Admin role
-        let mythicc_role = self
-            .guild_id
-            .create_role(&ctx.http, |r| {
-                r.hoist(true)
-                    .name("MythiccBot")
-                    .permissions(Permissions::ADMINISTRATOR)
-                    .position(lowest_admin_position as u8)
-            })
-            .await
-            .unwrap();
-
-        // Save the new `MythiccBot` role id inside redis
-        match redis_client::set_bot_role(connection, mythicc_role.id.to_string()) {
-            Ok(_) => println!("Bot Admin Role Process Set!"),
-            Err(e) => panic!("{}", e),
-        }
-    }
-
     async fn check_follower_role(&self, connection: &mut redis::Connection) {
         let follower_id = RoleId(
             env::var("ROLE_FOLLOWER_ID")
